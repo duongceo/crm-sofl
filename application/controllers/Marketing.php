59b5075baa9ad3fb81ff4cf64ae3c0e397144918
QuangNV <nv.quang.2897@gmail.com>
2020-11-20 14:27:29 +0700

    local

--- application/controllers/Marketing.php
+++ application/controllers/Marketing.php
@@ -156,7 +156,7 @@ class Marketing extends MY_Controller {
 		if (isset($get['tic_report'])) {
 			$typeReport = array(
 				'C3' => array(
-					'where' => array('is_old' => '0', 'source_id IN (1, 2, 6, 8)' => 'NO-VALUE', 'date_rgt >=' => $date_from, 'date_rgt <=' => $date_end),
+					'where' => array('is_old' => '0', 'duplicate_id' => '0', 'source_id IN (1, 2, 6, 8)' => 'NO-VALUE', 'date_rgt >=' => $date_from, 'date_rgt <=' => $date_end),
 				),
 				'L5' => array(
 					'where' => array('is_hide' => '0', 'is_old' => '0', 'source_id IN (1, 2, 6, 8)', 'level_contact_id' => 'L5', 'date_rgt >=' => $date_from, 'date_rgt <=' => $date_end),
@@ -165,7 +165,7 @@ class Marketing extends MY_Controller {
 		} else {
 			$typeReport = array(
 				'C3' => array(
-					'where' => array('is_old' => '0', 'source_id IN (1, 2, 6, 8)' => 'NO-VALUE', 'date_rgt >=' => $date_from, 'date_rgt <=' => $date_end),
+					'where' => array('is_old' => '0', 'duplicate_id' => '0', 'source_id IN (1, 2, 6, 8)' => 'NO-VALUE', 'date_rgt >=' => $date_from, 'date_rgt <=' => $date_end),
 				),
 				'L5' => array(
 					'where' => array('is_hide' => '0', 'is_old' => '0', 'source_id IN (1, 2, 6, 8)', 'level_contact_id' => 'L5', 'date_rgt_study >=' => $date_from, 'date_rgt_study <=' => $date_end),
