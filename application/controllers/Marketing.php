be4a7e98eccad05a453f4e3f4600f8614997ea00
QuangNV <nv.quang.2897@gmail.com>
2021-03-19 17:25:48 +0700

    local to server

--- application/controllers/Marketing.php
+++ application/controllers/Marketing.php
@@ -166,7 +166,7 @@ class Marketing extends MY_Controller {
 		} else {
 			$typeReport = array(
 				'C3' => array(
-					'where' => array('is_old' => '0','call_status_id NOT IN (1, 3)' => 'NO-VALUE', 'level_contact_detail NOT IN ("L1.1", "L1.2", "L1.3")' => 'NO-VALUE', 'source_id IN (1, 2, 6, 8)' => 'NO-VALUE', 'date_rgt >=' => $date_from, 'date_rgt <=' => $date_end),
+					'where' => array('is_old' => '0', 'call_status_id NOT IN (1, 3)' => 'NO-VALUE', 'level_contact_detail NOT IN ("L1.1", "L1.2", "L1.3")' => 'NO-VALUE', 'source_id IN (1, 2, 6, 8)' => 'NO-VALUE', 'date_rgt >=' => $date_from, 'date_rgt <=' => $date_end),
 				),
 				'L5' => array(
 					'where' => array('is_hide' => '0', 'is_old' => '0', 'source_id IN (1, 2, 6, 8)' => 'NO-VALUE', 'level_contact_id' => 'L5', 'date_rgt_study >=' => $date_from, 'date_rgt_study <=' => $date_end),
@@ -394,7 +394,7 @@ class Marketing extends MY_Controller {
 
 		if ($this->role_id == 6) {
 			$data['left_col'] = array('date_happen_1', 'tic_report');
-//				$data['right_col'] = array('channel');
+			$data['right_col'] = array('is_old');
 		} else {
 			$data['left_col'] = array('date_happen_1', 'tic_report', 'marketer');
 			$data['right_col'] = array('branch');
