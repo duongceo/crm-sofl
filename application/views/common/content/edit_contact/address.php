828cf453dc155ee6fe4891d6ecae221e4899a038
QuangNV <nv.quang.2897@gmail.com>
2021-07-22 17:09:06 +0700

    local to server

--- application/views/common/content/edit_contact/address.php
+++ application/views/common/content/edit_contact/address.php
@@ -1,9 +1,17 @@
-<tr>
-    <td class="text-right">  Địa chỉ </td>
-    <td>  
-        <div class="form-group">
-            <label for="address" class="sr-only">Address</label>
-            <textarea class="form-control" rows="3" name="address"> <?php echo $rows['address']; ?></textarea>
-        </div>
-    </td>
+<tr>
+
+    <td class="text-right">  Địa chỉ </td>
+
+    <td>  
+
+        <div class="form-group">
+
+            <label for="address" class="sr-only">Address</label>
+
+            <textarea class="form-control" rows="2" name="address"> <?php echo $rows['address']; ?></textarea>
+
+        </div>
+
+    </td>
+
 </tr>
