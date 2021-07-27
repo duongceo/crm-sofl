6f64dd368b2ddc120f6c74895be4f91e4a288a73
QuangNV <nv.quang.2897@gmail.com>
2021-07-27 11:31:35 +0700

    local to server

--- application/views/common/content/table/head/fee.php
+++ application/views/common/content/table/head/fee.php
@@ -1,8 +1,6 @@
 <th class="order_price_purchase tbl_price_purchase" id="th_tbl_price_purchase">
 
-    <input type="text" class="order_price_purchase hidden" name="order_price_purchase"
-
-           value="<?php echo (isset($_GET['order_price_purchase'])) ? $_GET['order_price_purchase'] : '0'; ?>" />
+    <input type="text" class="order_price_purchase hidden" name="order_price_purchase"/>
 
 			Học phí
 
---
+++ application/views/common/content/table/head/fee_missing.php
@@ -0,0 +1,7 @@
+<th class="order_price_purchase tbl_price_purchase" id="th_tbl_price_purchase">
+
+	<input type="text" class="hidden" />
+
+	Học phí còn lại
+
+</th>
