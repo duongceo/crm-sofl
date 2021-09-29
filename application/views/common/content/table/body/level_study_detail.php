3740b96ba3f834800ccdd9e66149b3e8629869d4
Ngo_Quang <nv.quang.2897@gmail.com>
2021-09-29 16:46:09 +0700

    from local

--- application/views/common/content/table/body/level_study_detail.php
+++ application/views/common/content/table/body/level_study_detail.php
@@ -2,7 +2,7 @@
 	<?php if (isset($value['level_study_detail']) && $value['level_study_detail'] != '') {
 		echo $value['level_study_detail'];
 	} else if (isset($value['level_study_id'])) {
-		echo $value['level_study_detail'];
+		echo $value['level_study_id'];
 	}
 	?>
 </td>
