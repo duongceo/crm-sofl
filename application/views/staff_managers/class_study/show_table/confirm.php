7c13ead9450ae3433e17f3e81b9becb9cc49ca3d
Ngo_Quang <nv.quang.2897@gmail.com>
2022-03-05 15:16:15 +0700

    local to serverr

--- application/views/staff_managers/class_study/show_table/confirm.php
+++ application/views/staff_managers/class_study/show_table/confirm.php
@@ -2,11 +2,12 @@
     <?php if (!empty($row['user_confirm'])) { ?>
         <p class="bg-success"> Đã xác nhận </p>
     <?php } else { ?>
-        <p class="bg-danger"> Chưa xác nhận </p>
         <?php if ($this->role_id == 3) { ?>
             <button style="margin-right: 0" type="button" class="btn btn-success confirm_class">
-            Xác nhận
+                Xác nhận
             </button>
+        <?php } else { ?>
+            <p class="bg-danger"> Chưa xác nhận </p>
         <?php } ?>
     <?php } ?>
 </td>
